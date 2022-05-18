4df820b1e5678ff6610cc6ec3123086325265918
Zzney <zzulpukarov@yandex.ru>
2022-05-18 13:56:01 +0300

    les 85 request HTTP

--- lib/recent_calls/call_info_screen.dart
+++ lib/recent_calls/call_info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CallInfo extends StatelessWidget {
   const CallInfo({Key? key}) : super(key: key);
@@ -7,10 +8,19 @@ class CallInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.orange[300],
-        alignment: Alignment.center,
-        child: const Text("Меня зовут Зулпукар"),
-      ),
+          color: Colors.orange[300],
+          alignment: Alignment.center,
+          child: FutureBuilder<http.Response>(
+            future: http.get(Uri.parse('https://catfact.ninja/fact')),
+            builder:
+                (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.body);
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          )),
     );
   }
 }
